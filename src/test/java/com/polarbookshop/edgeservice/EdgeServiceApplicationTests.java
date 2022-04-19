2e160a7d1210e1cda27b0c64e0704e9ebcce6b1f
jkannar <janos.kannar@gmail.com>
2022-04-19 18:14:40 +0200

    [TEST] Redis test container

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class EdgeServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	private static final int REDIS_PORT = 6379;
 
+	@Container
+	static GenericContainer<?> redis =
+		new GenericContainer<>(DockerImageName.parse("redis:6.2")).withExposedPorts(REDIS_PORT);
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", () -> redis.getHost());
+		registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+	}
+
+	@Test
+	void verifyThatSpringContextLoads() {
+	}
 }
